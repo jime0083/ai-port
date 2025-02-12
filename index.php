4d071c4e54de17639e0141c70cbc5926e3b7589d
jime <kuritahajime.business@gmail.com>
2025-02-13 03:08:05 +0900

    Cursurを使用してのWeb政策テスト

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ウェブサイト</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#top">TOP</a></li>
+                <li><a href="#info">INFO</a></li>
+                <li><a href="#news">NEWS</a></li>
+                <li><a href="#services">SERVICES</a></li>
+                <li><a href="#work">WORK</a></li>
+                <li><a href="#recruit">RECRUIT</a></li>
+            </ul>
+        </nav>
+    </header>
