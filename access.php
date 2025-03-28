95b7f2ffbd157e77b45bd88c05d4f26fe3983cb5
jime <kuritahajime.business@gmail.com>
2025-03-28 15:24:16 +0900

    Accessページ追加

---
+++ access.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Template Name: Access
+*/
+get_header(); ?>
+
+<div class="page-header access-header">
+    <div class="page-header-overlay"></div>
+    <h1 class="page-title">ACCESS</h1>
+</div>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d22450.22666203898!2d139.7293971382098!3d35.67860302761904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sja!2sjp!4v1743142262281!5m2!1sja!2sjp"height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+<section class="access-info-section">
+    <div class="access-info-container">
+        <div class="access-basic-info">
+            <div class="info-row">
+                <p class="address">〒 921-8045　石川県金沢市大桑3丁目56</p>
+                <p class="tel">TEL.076-287-6653(Barns直通)</p>
+            </div>
+            <div class="info-row">
+                <p class="cafe">café Musee　Last Order.17:00</p>
+                <p class="hours">OPEN.10:00　CLOSE.18:00　木曜定休</p>
+            </div>
+        </div>
+
+        <div class="access-routes">
+            <div class="route-section">
+                <h2 class="route-title">車をご利用の場合</h2>
+                <ul class="route-list">
+                    <li>山側環状線の大桑出口を降りて100mほど、やまざき養蜂場さんが左手に見えたらその先の交差点を右折してすぐです。</li>
+                    <li>144号線野田町交差点を右折し、笠舞方面へ。大桑町東交差点を右折し、右手に洋服の青山さんが見えたらその先の交差点を左折してすぐです。</li>
+                </ul>
+            </div>
+
+            <div class="route-section">
+                <h2 class="route-title">北陸鉄道バスをご利用の場合</h2>
+                <ul class="route-list">
+                    <li>金沢駅西口4番乗り場 19番線「大桑本町」行き、「大桑タウン」下車。</li>
+                    <li>金沢駅東口7番乗り場 22番線「大桑住宅」行き、終点「大桑住宅」下車。</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="contact-section">
+            <div class="contact-overlay"></div>
+                <div class="contact-container">
+                    <p class="contact-label">お問い合わせ</p>
+                    <h2 class="contact-title">CONTACT</h2>
+                    <p class="contact-description">
+                        外構工事・お庭のご依頼はこちらからお気軽にご連絡ください。お問い合わせの多いご質問や、よくいただくご質問は別途「よくあるご質問」ページに掲載しておりますので、ご活用ください。
+                    </p>
+                    <a href="#" class="contact-button">
+                        <span class="circle-arrow">→</span>
+                    </a>
+                </div>
+        </section>
+
+<?php get_footer(); ?>
